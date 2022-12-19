28917fc12c3e54d28239b23f4f285ae7a7b26a97
pszczeszynski <pszczesz@uwaterloo.ca>
2022-12-18 22:56:39 -0500

    Add custom robot script for Flipsy. Run after SinglePlayer

--- Assets/Scripts/BB_RobotControllerLink.cs
+++ Assets/Scripts/BB_RobotControllerLink.cs
@@ -1,38 +1,44 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEditor;
 
 
-public class BB_robotControllerIP : MonoBehaviour
+public class BB_RobotControllerLink
 {
-    private string robotControllerIP = "127.0.0.1";
-    private int robotControllerPort = 11115;
-
+    private string robotControllerIP;
+    private int robotControllerPort;
     private UdpClient udpClient;
-
     private IPEndPoint robotControllerEP;
 
-    void init() {
-        robotControllerEP = new IPEndPoint(IPAddress.Parse(
-            robotControllerIP), robotControllerPort); 
+    public BB_RobotControllerLink(string robotControllerIP = "127.0.0.1",
+                                  int robotControllerPort = 11115)
+    {
+        this.robotControllerIP = robotControllerIP;
+        this.robotControllerPort = robotControllerPort;
+        robotControllerEP = new IPEndPoint(
+            IPAddress.Parse(robotControllerIP), robotControllerPort);
+        udpClient = new UdpClient();
         udpClient.Connect(robotControllerEP);
     }
 
-    void Update() {
-        string messageString = "hello";
-        byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
-        
+    public void SendMessage(string message)
+    {
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
         udpClient.Send(messageBytes, messageBytes.Length);
+    }
+
+    // returns the latest requested control from the server
+    public ControlInput GetLatestControlInput()
+    {
+        return new ControlInput { drive_amount = 1.0, turn_amount = 0.0 };
     }
 }
 
+public struct ControlInput
+{
+    public double drive_amount;
+    public double turn_amount;
+}
 
 
 
---
+++ Assets/Scripts/Robots/Robot_BB_Flipsy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+public class Robot_BB_Flipsy : RobotInterface3D {
+    BB_RobotControllerLink robotControllerLink = new BB_RobotControllerLink("127.0.0.1", 11115);
+
+    public void Awake()
+    {
+        
+    }
+
+    public override void Update_Robot()
+    {
+        robotControllerLink.SendMessage("Hello");
+        ControlInput input = robotControllerLink.GetLatestControlInput();
+
+        gamepad1_left_stick_y = (float) input.drive_amount;
+        gamepad1_right_stick_x = (float) input.turn_amount;
+    }
+}
