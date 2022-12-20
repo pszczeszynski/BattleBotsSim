65e61fa27aef3723450a750f97bbdd51f665a683
pszczeszynski <pszczesz@uwaterloo.ca>
2022-12-20 10:53:12 -0500

    Refactor udp receiver into class

--- RobotController/RobotController.cc
+++ RobotController/RobotController.cc
@@ -4,22 +4,38 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define DEFAULT_PORT "11115"
 #define DEFAULT_BUFLEN 512
 
-int main()
+class ServerSocket
+{
+private:
+    std::string port;
+    SOCKET listenSocket;
+    int setup_receiving_socket();
+
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    sockaddr_storage last_sender_addr;
+    socklen_t last_sender_addr_len;
+
+public:
+    std::string receive();
+    void reply_to_last_sender(std::string);
+
+    ServerSocket(std::string port) : port(port), listenSocket(INVALID_SOCKET)
+    {
+        setup_receiving_socket();
+    }
+};
+
+int ServerSocket::setup_receiving_socket()
 {
     WSADATA wsaData;
     int iResult;
 
-    SOCKET ListenSocket = INVALID_SOCKET;
-
-    struct addrinfo *result = NULL;
-    struct addrinfo hints;
-
-    int iSendResult;
-    char recvbuf[DEFAULT_BUFLEN];
-    int recvbuflen = DEFAULT_BUFLEN;
+    addrinfo *result = NULL;
+    addrinfo hints;
 
     // Initialize Winsock
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -35,8 +51,8 @@ int main()
     hints.ai_protocol = IPPROTO_UDP;
     hints.ai_flags = AI_PASSIVE;
 
-    // Resolve the server address and port
-    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    // Resolve the server address and port. Returns 0 on success or an error
+    iResult = getaddrinfo(NULL, port.c_str(), &hints, &result);
     if (iResult != 0)
     {
         std::cerr << "getaddrinfo failed with error: " << iResult << std::endl;
@@ -45,8 +61,8 @@ int main()
     }
 
     // Create a SOCKET for connecting to server
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET)
+    listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (listenSocket == INVALID_SOCKET)
     {
         std::cerr << "socket failed witherror: " << WSAGetLastError() << std::endl;
         freeaddrinfo(result);
@@ -55,34 +71,53 @@ int main()
     }
 
     // Setup the UDP listening socket
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    iResult = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR)
     {
         std::cerr << "bind failed with error: " << WSAGetLastError() << std::endl;
         freeaddrinfo(result);
-        closesocket(ListenSocket);
+        closesocket(listenSocket);
         WSACleanup();
         return 1;
     }
 
     freeaddrinfo(result);
 
-    // Receive until the peer closes the connection
+    return 0;
+}
+
+std::string ServerSocket::receive()
+{
+    last_sender_addr_len = sizeof(last_sender_addr);
+    int numBytesReceived = recvfrom(listenSocket, recvbuf, recvbuflen, 0,
+                                    (sockaddr *)&last_sender_addr, &last_sender_addr_len);
+
+    std::string ret = "";
+    if (numBytesReceived > 0)
+    {
+        ret.assign(recvbuf, numBytesReceived);
+    }
+
+    return ret;
+}
+
+void ServerSocket::reply_to_last_sender(std::string data) {
+
+    int iSendResult = sendto(listenSocket, data.c_str(), data.length(), 0, (sockaddr *)&last_sender_addr, last_sender_addr_len);
+    if (iSendResult == SOCKET_ERROR)
+    {
+        std::cerr << "sendto failed with error: " << WSAGetLastError() << std::endl;
+    }
+}
+
+int main()
+{
+    ServerSocket socket{"11115"};
+    // receive until the peer closes the connection
     while (true)
     {
-        struct sockaddr_storage sender_addr;
-        socklen_t addr_len = sizeof(sender_addr);
-        iResult = recvfrom(ListenSocket, recvbuf, recvbuflen, 0, (struct sockaddr *)&sender_addr, &addr_len);
-        if (iResult > 0)
-        {
-            std::cout << "Bytes received: " << iResult << std::endl;
-            // Echo the buffer back to the sender
-            iSendResult = sendto(ListenSocket, recvbuf, iResult, 0, (struct sockaddr *)&sender_addr, addr_len);
-
-            if (iSendResult == SOCKET_ERROR)
-            {
-                std::cerr << "sendto failed with error: " << WSAGetLastError() << std::endl;
-            }
-        }
+        std::string received = socket.receive();
+        if (received == "") continue;
+        socket.reply_to_last_sender(received);
     }
 }
