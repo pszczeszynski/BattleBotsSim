e6e8d2f9a4b2a47ccfd11dc378eb525b711754e1
pszczeszynski <pszczesz@uwaterloo.ca>
2022-12-28 10:25:45 -0500

    Server talks to robot controller, not client

--- FTC_Simulator/Assets/Scripts/ClientLow.cs
+++ FTC_Simulator/Assets/Scripts/ClientLow.cs
@@ -2117,12 +2117,7 @@ public class ClientLow : MonoBehaviour
             return;
         }
 
-        // don't override flipsy's gamepad varaibles since it is controlled externally
-        // flipsy will decide when to call this
-        if (!(myRobot_working is Robot_BB_Flipsy))
-        {
-            myRobot_working.updateGamepadVars();
-        }
+        myRobot_working.updateGamepadVars();
 
         if ( myPosition == "Spectator" ) { return; }
 
--- FTC_Simulator/Assets/Scripts/Robots/Robot_BB_Flipsy.cs
+++ FTC_Simulator/Assets/Scripts/Robots/Robot_BB_Flipsy.cs
@@ -30,18 +30,17 @@ public class Robot_BB_Flipsy : RobotInterface3D
     private const float ENEMY_SEARCH_INTERVAL_SECONDS = 2.0f;
     public void Awake()
     {
-        // don't execute this on the server
-        if (!GLOBALS.CLIENT_MODE) { return; }
+        // don't execute this on the client
+        if (GLOBALS.CLIENT_MODE) { return; }
+        UnityEngine.Debug.Log("in awake, not in client mode!");
         _robotControllerLink = new BB_RobotControllerLink("127.0.0.1", 11115);
         InvokeRepeating("CacheEnemiesAndChooseOneToTrack", 0.0f, ENEMY_SEARCH_INTERVAL_SECONDS);
     }
 
     public override void Update_Robot()
     {
-        // don't execute this on the server
-        if (!GLOBALS.CLIENT_MODE) { return; }
-
-        updateGamepadVars();
+        // don't execute this on the client
+        if (GLOBALS.CLIENT_MODE) { return; }
 
         UnityEngine.Debug.Log("in update_robot()");
         BattleBotState state = new BattleBotState
@@ -62,6 +61,7 @@ public class Robot_BB_Flipsy : RobotInterface3D
         // apply the control input to the robot
         if (input.HasValue)
         {
+            UnityEngine.Debug.Log("turn_amount received: " + input.Value.turn_amount);
             gamepad1_left_stick_y = (float)input.Value.drive_amount;
             gamepad1_right_stick_x = (float)input.Value.turn_amount;
         }
@@ -72,7 +72,7 @@ public class Robot_BB_Flipsy : RobotInterface3D
     private void CacheEnemiesAndChooseOneToTrack()
     {
         // find all enemy robot bo transforms.
-        // order by distance too us
+        // order by distance to us
         _enemyRobotBodies = FindObjectsOfType<RobotInterface3D>()
                 .Select(c => c.transform)
                 .Where(c => c != this.transform)
@@ -80,7 +80,6 @@ public class Robot_BB_Flipsy : RobotInterface3D
                 .OrderBy(c => Vector3.Distance(robot_body.position, c.position))
                 .ToList();
 
-        Debug.Log("enemy robot bodies count: " + _enemyRobotBodies.Count);
         // choose the closest to track
         if (_enemyRobotBodies.Count > 0)
         {
