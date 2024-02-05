a044c7079e3cc62e97f7f94fc767e77d7fab5514
TongguangZhang <tonggz250@gmail.com>
2024-02-05 11:00:26 -0500

    created CVPosition class

--- RobotController/CVPosition.cc
+++ RobotController/CVPosition.cc
@@ -1,21 +1,73 @@
 #include "CVPosition.h"
-
+#include <opencv2/dnn.hpp>
+#include <opencv2/core.hpp>
+#include <iostream>
 
 CVPosition::CVPosition()
 {
     // load the model
     _net = cv::dnn::readNetFromONNX(MODEL_PATH);
+    modelShape = cv::Size(1280, 1280);
 }
-
 CVPosition& CVPosition::GetInstance()
 {
     static CVPosition instance;
     return instance;
 }
 
-cv::Point2f CVPosition::ComputeRobotPosition(cv::Mat& fieldImage)
+std::vector<int> CVPosition::ComputeRobotPosition(cv::Mat& fieldImage)
 {
-    // todo
+    cv::Mat img = fieldImage;
 
-    return cv::Point2f(0, 0);
+    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+    cv::merge(std::vector<cv::Mat>{img, img, img}, img);
+    cv::resize(img, img, cv::Size(1280, 1280));
+
+    img.convertTo(img, CV_32F);
+    img /= 255.0;
+
+    cv::Mat blob;
+    cv::dnn::blobFromImage(img, blob, 1.0);
+    _net.setInput(blob);
+
+    std::vector<cv::Mat> outputs;
+    _net.forward(outputs, _net.getUnconnectedOutLayersNames());
+
+    cv::Mat predictions = outputs[0];
+
+    int rows = predictions.size[2];
+    int dimensions = predictions.size[1];
+    
+    predictions = predictions.reshape(1, dimensions);
+    cv::transpose(predictions, predictions);
+
+    float *data = (float *)predictions.data;
+
+    int x;
+    int y;
+    int w;
+    int h;
+
+    float max_score = 0;
+
+    for (int i = 0; i < rows; i++) {
+        float score = data[4];
+        if (score > max_score) { 
+            max_score = score;
+            x = int(data[0]);
+            y = int(data[1]);
+            w = int(data[2]);
+            h = int(data[3]);
+        }
+        data += dimensions;
+    }
+
+    x = x * 720 / 1280;
+    y = y * 720 / 1280;
+    w = w * 720 / 1280;
+    h = h * 720 / 1280;
+
+    std::vector<int> boundingBox = {x, y, w, h};
+
+    return boundingBox;
 }
--- RobotController/CVPosition.h
+++ RobotController/CVPosition.h
@@ -2,7 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 
-#define MODEL_PATH "MachineLearning/position_model.onnx"
+#define MODEL_PATH "MachineLearning/best.onnx"
 
 class CVPosition
 {
@@ -10,8 +10,19 @@ public:
     CVPosition();
     static CVPosition& GetInstance();
 
-    cv::Point2f ComputeRobotPosition(cv::Mat& fieldImage);
+    std::vector<int> ComputeRobotPosition(cv::Mat& fieldImage);
 private:
     cv::dnn::Net _net;
     cv::Point2f _lastPosition;
+
+    std::vector<std::string> classes{"orbitron"};
+
+    cv::Size2f modelShape{};
+
+    float modelConfidenceThreshold {0.25};
+    float modelScoreThreshold      {0.45};
+    float modelNMSThreshold        {0.50};
+
+    bool letterBoxForSquare = true;
+
 };
