6a822936153efb819fb475400f2a931460d65570
Orbitron <pszczesz@uwaterloo.ca>
2024-02-19 17:00:53 -0500

    Fixup input state bug + add mouse buttons

--- RobotController/Input/InputState.h
+++ RobotController/Input/InputState.h
@@ -10,17 +10,23 @@ public:
     InputState() = default;
 
     static InputState& GetInstance();
-    // Update the key state; should be called from the UI thread
-    void UpdateKeyState(int key, bool isDown);
     // Check if a key is down; can be called from any thread
     bool IsKeyDown(ImGuiKey key) const;
+
+    bool IsMouseDown(int button) const;
 
     // Update all key states based on ImGui; should be called from the UI thread
     void UpdateAllKeyStates();
 
 private:
+    // Update the key state; should be called from the UI thread
+    void UpdateKeyState(int key, bool isDown);
+    void UpdateMouseButtonState(int button, bool isDown);
+
     mutable std::mutex mutex_; // mutable because IsKeyDown is const
     std::array<bool, 100> keyStates_{}; // Adjust the size based on your needs
 
     static InputState instance;
+
+    std::array<bool, 3> mouseButtonStates_{};
 };
