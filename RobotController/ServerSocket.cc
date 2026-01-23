c4a21ca9d13d9bf34cd9f330cb2d596db9cd529f
pszczeszynski <pszczesz@uwaterloo.ca>
2026-01-23 01:23:16 -0800

    Formatting

--- RobotController/ServerSocket.cc
+++ RobotController/ServerSocket.cc
@@ -1,121 +1,110 @@
 // ServerSocket.cpp
 #include "ServerSocket.h"
-#include <string>
+
 #include <iostream>
+#include <string>
+
 
 /**
  * Constructor
  * @param port The port to listen on
  */
 ServerSocket::ServerSocket(std::string port)
-    : port(port), listenSocket(INVALID_SOCKET)
-{
-    setup_receiving_socket();
+    : port(port), listenSocket(INVALID_SOCKET) {
+  setup_receiving_socket();
 }
 
-ServerSocket::~ServerSocket()
-{
-    closesocket(listenSocket);
-    WSACleanup();
+ServerSocket::~ServerSocket() {
+  closesocket(listenSocket);
+  WSACleanup();
 }
 
 /**
  * Setup the socket to receive messages
  */
-int ServerSocket::setup_receiving_socket()
-{
-    WSADATA wsaData;
-    int iResult;
+int ServerSocket::setup_receiving_socket() {
+  WSADATA wsaData;
+  int iResult;
 
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0)
-    {
-        std::cerr << "WSAStartup failed with error: " << iResult << std::endl;
-        return 1;
-    }
+  // Initialize Winsock
+  iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != 0) {
+    std::cerr << "WSAStartup failed with error: " << iResult << std::endl;
+    return 1;
+  }
 
-    SOCKADDR_IN serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(std::stoi(port));
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+  SOCKADDR_IN serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(std::stoi(port));
+  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // Create a SOCKET for listening
-    listenSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (listenSocket == INVALID_SOCKET)
-    {
-        std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
+  // Create a SOCKET for listening
+  listenSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (listenSocket == INVALID_SOCKET) {
+    std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
+    WSACleanup();
+    return 1;
+  }
 
-    // Bind the listening socket to the specified port
-    if (bind(listenSocket, (SOCKADDR *)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
-    {
-        std::cerr << "bind failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
+  // Bind the listening socket to the specified port
+  if (bind(listenSocket, (SOCKADDR *)&serverAddress, sizeof(serverAddress)) ==
+      SOCKET_ERROR) {
+    std::cerr << "bind failed with error: " << WSAGetLastError() << std::endl;
+    closesocket(listenSocket);
+    WSACleanup();
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
 
 /**
  * Receive a message from the socket
  * @return The message received, otherwise an empty string
  */
-std::string ServerSocket::receive(int* outError)
-{
-    last_sender_addr_len = sizeof(last_sender_addr);
-    std::string ret = "";
+std::string ServerSocket::receive(int *outError) {
+  last_sender_addr_len = sizeof(last_sender_addr);
+  std::string ret = "";
 
-    // Set the socket to non-blocking mode
-    u_long iMode = 1;
-    ioctlsocket(listenSocket, FIONBIO, &iMode);
+  // Set the socket to non-blocking mode
+  u_long iMode = 1;
+  ioctlsocket(listenSocket, FIONBIO, &iMode);
 
-    // Read all available messages and only keep the last one
-    while (true)
-    {
-        int numBytesReceived = recvfrom(listenSocket, recvbuf, recvbuflen, 0, (SOCKADDR *)&last_sender_addr, &last_sender_addr_len);
+  // Read all available messages and only keep the last one
+  while (true) {
+    int numBytesReceived =
+        recvfrom(listenSocket, recvbuf, recvbuflen, 0,
+                 (SOCKADDR *)&last_sender_addr, &last_sender_addr_len);
 
-        if (numBytesReceived == SOCKET_ERROR)
-        {
-            int err = WSAGetLastError();
+    if (numBytesReceived == SOCKET_ERROR) {
+      int err = WSAGetLastError();
 
-            if (err == WSAEWOULDBLOCK)
-            {
-                // No more messages
-                break;
-            }
-            else
-            {
-                // Error occurred
-                std::cerr << "recvfrom on port " << port << " failed with error: " << err << std::endl;
-                if (outError != nullptr)
-                {
-                    *outError = err;
-                    return "";
-                }
-
-                break;
-
-            }
+      if (err == WSAEWOULDBLOCK) {
+        // No more messages
+        break;
+      } else {
+        // Error occurred
+        std::cerr << "recvfrom on port " << port
+                  << " failed with error: " << err << std::endl;
+        if (outError != nullptr) {
+          *outError = err;
+          return "";
         }
-        else if (numBytesReceived > 0)
-        {
-           
-            ret.assign(recvbuf, numBytesReceived);
-            // Update last_sender_addr and last_sender_addr_len
-            // This is already handled by recvfrom parameters
-        }
+
+        break;
+      }
+    } else if (numBytesReceived > 0) {
+      ret.assign(recvbuf, numBytesReceived);
+      // Update last_sender_addr and last_sender_addr_len
+      // This is already handled by recvfrom parameters
     }
+  }
 
-    // Set the socket back to blocking mode
-    iMode = 0;
-    ioctlsocket(listenSocket, FIONBIO, &iMode);
+  // Set the socket back to blocking mode
+  iMode = 0;
+  ioctlsocket(listenSocket, FIONBIO, &iMode);
 
-    return ret;
+  return ret;
 }
 
 /**
@@ -123,17 +112,19 @@ std::string ServerSocket::receive(int* outError)
  *
  * @param data The string to send
  */
-void ServerSocket::reply_to_last_sender(std::string data)
-{
-    if((listenSocket == INVALID_SOCKET) || (last_sender_addr_len == 0) || (data.length() > 65400) ) // leave room for unknown overhead
-    {
-        return;
-    }
+void ServerSocket::reply_to_last_sender(std::string data) {
+  if ((listenSocket == INVALID_SOCKET) || (last_sender_addr_len == 0) ||
+      (data.length() > 65400))  // leave room for unknown overhead
+  {
+    return;
+  }
 
-    int iSendResult = sendto(listenSocket, data.c_str(), data.length(), 0, (SOCKADDR *)&last_sender_addr, last_sender_addr_len);
+  int iSendResult = sendto(listenSocket, data.c_str(), data.length(), 0,
+                           (SOCKADDR *)&last_sender_addr, last_sender_addr_len);
 
-    // if (iSendResult == SOCKET_ERROR)
-    // {
-    //     std::cerr << "sendto failed with error: " << WSAGetLastError() << std::endl;
-    // }
+  // if (iSendResult == SOCKET_ERROR)
+  // {
+  //     std::cerr << "sendto failed with error: " << WSAGetLastError() <<
+  //     std::endl;
+  // }
 }
--- RobotController/ServerSocket.h
+++ RobotController/ServerSocket.h
@@ -4,35 +4,32 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#include <iostream>
-
 #define DEFAULT_BUFLEN 512
 
-class ServerSocket
-{
-private:
-    // The port to listen on
-    std::string port;
-    // The socket to listen on
-    SOCKET listenSocket;
-    // Buffer for receiving data
-    char recvbuf[DEFAULT_BUFLEN];
-    // Number of bytes actually received
-    int recvbuflen = DEFAULT_BUFLEN;
-    // The address of the last sender
+class ServerSocket {
+ private:
+  // The port to listen on
+  std::string port;
+  // The socket to listen on
+  SOCKET listenSocket;
+  // Buffer for receiving data
+  char recvbuf[DEFAULT_BUFLEN];
+  // Number of bytes actually received
+  int recvbuflen = DEFAULT_BUFLEN;
+  // The address of the last sender
 
+  int setup_receiving_socket();
 
-    int setup_receiving_socket();
+ public:
+  // The address of the last sender
+  // Made public for now so human interface can prevent it from changing if a
+  // bad message is received
+  SOCKADDR last_sender_addr;
+  // The length of the last sender's address
+  int last_sender_addr_len;
 
-public:
-    // The address of the last sender
-    // Made public for now so human interface can prevent it from changing if a bad message is received
-    SOCKADDR last_sender_addr;
-    // The length of the last sender's address
-    int last_sender_addr_len;
-
-    std::string receive(int* outError = nullptr);
-    void reply_to_last_sender(std::string data);
-    ServerSocket(std::string port);
-    ~ServerSocket();
+  std::string receive(int* outError = nullptr);
+  void reply_to_last_sender(std::string data);
+  ServerSocket(std::string port);
+  ~ServerSocket();
 };
